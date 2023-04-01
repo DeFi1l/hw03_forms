b14e52af0840bc03b5bb21740d8a866fd451f751
Денис Филлер <denis.filler@yandex.ru>
2023-04-01 19:24:53 +0300

    added labels to forms

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -8,6 +8,7 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
+        labels = {'text': 'Текст', 'group': 'Группа'}
 
         help_texts = {
             'text': 'Текст нового поста',
